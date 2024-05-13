ef909a84b1f9895d295b2d12a3d985cc72dba96e
Sushant Butta <sushant.butta@gmail.com>
2024-05-13 23:44:39 +0530

    Initial commit

---
+++ src/main/java/org/buttas/Main.java
@@ -0,0 +1,16 @@
+package org.buttas;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Adding Three Numbers!!");
+
+        byte a = 1;
+        byte b = 2;
+        byte c = 3;
+
+        byte  d =  (byte) (a+b+c);
+
+        System.out.println("Output is :" + d );
+
+    }
+}
